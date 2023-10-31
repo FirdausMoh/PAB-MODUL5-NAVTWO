3251959f7d2f827d0619e0c1a9a9dc73cbb4579b
Fahri Firdaus <mohamadfahri302@gmail.com>
2023-10-31 10:34:32 +0700

    .

--- app/news-detail.js
+++ app/news-detail.js
@@ -1,3 +1,26 @@
+// import { Heading, Text, Center } from "@gluestack-ui/themed";
+// import { Header } from "../components";
+// import { useLocalSearchParams } from "expo-router";
+
+// const NewsDetail = () => {
+//   const params = useLocalSearchParams();
+//   return (
+//     <>
+//       <Header title={"News"} withBack={true} />
+//       <Center flex={1} padding={"$4"}>
+//         <Heading>News Detail</Heading>
+//         <Text textAlign="center">{params.title}</Text>
+//       </Center>
+//     </>
+//   );
+// };
+
+// export default NewsDetail;
+
+
+//chalenge Edit dan sesuaikan Screen News Detail 
+//sehingga dapat menampilkan image, date, title dan content berdasarkan News yang diklik
+
 import { Heading, Text, Image, ScrollView, Divider } from "@gluestack-ui/themed";
 import { Header } from "../components";
 import { useLocalSearchParams } from "expo-router";
